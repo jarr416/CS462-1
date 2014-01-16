7a1e829ae3f81aa3bcd23b6e5fcd2670d1a4bd16
Pete <petercope@gmail.com>
2014-01-16 09:49:06 -0700

    Update java.sh

--- scripts/java.sh
+++ scripts/java.sh
@@ -3,3 +3,5 @@
 sudo apt-get install openjdk-7-jdk -y
 update-java-alternatives -l
 sudo update-java-alternatives -s java-1.7.0-openjdk
+JAVA_HOME="/usr/lib/jvm/java-1.7.0-openjdk"
+JRE_HOME="/usr/lib/jvm/java-1.7.0-openjdk/jre"
